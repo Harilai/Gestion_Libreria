7cd5cc35088e57dd8c2da880df64a5d2ae4e09e1
Usuario <Usuario@DESKTOP-TA74P1I>
2025-05-14 12:29:19 +0200

    Principal Editada 1

--- src/Libreria/Principal.java
+++ src/Libreria/Principal.java
@@ -1,10 +1,43 @@
 package Libreria;
 
+import java.util.*;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner entrada = new Scanner(System.in); int opcion = 0;
+		
+		do {
+			System.out.println("-----BIENVENIDO A NUESTA LIBRERIA-----");
+			System.out.println(" \n ¿Que desea hacer? \n"
+					+ "1. Gestion Usuarios \n"
+					+ "2. Gestion Libros \n"
+					+ "3. Gestion Prestamos \n"
+					+ "4. Salir");
+			try {
+				
+			opcion = Integer.parseInt(entrada.nextLine());
+			
+			} catch (NumberFormatException ex) {
+				System.out.println("Error, formato no valido ( " +ex.getMessage()+ " )");
+			} catch (Exception ex) {
+				System.out.println("Error no esperado : " +ex.getMessage());
+			}
+			
+			switch(opcion) {
+			
+			
+			case 1 : 
+			}
+			
+			
+			
+			
+		} while (opcion !=4);
+		
+		
+		
 	}
 
 }
